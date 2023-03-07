ad3c075694a281c967b8bbf7cd57fd253d950f08
Your Name Martin <you@example.comMartinMaffei95@gmail.com>
2023-03-07 12:56:41 -0300

    added auto scroll

--- src/components/Container/ChatBox.container.tsx
+++ src/components/Container/ChatBox.container.tsx
@@ -1,13 +1,19 @@
+import { useRef, useEffect } from 'react';
 import { useSelector } from 'react-redux';
-import { Conversation } from '../../interfaces/Conversation.interface';
 import { ReduxState } from '../../interfaces/Redux.interface';
 import ChatBubble from '../ChatBubble';
 
 const ChatBox = () => {
   const { messages } = useSelector((state: ReduxState) => state.conversation);
-
+  const box = useRef<HTMLDivElement>(null);
+  useEffect(() => {
+    box.current?.scrollTo(0, box.current.scrollHeight);
+  }, [messages]);
   return (
-    <div className="flex flex-col flex-1 py-2 px-4 gap-2 mb-20 h-screen overflow-y-scroll">
+    <div
+      ref={box}
+      className="flex flex-col flex-1 py-2 px-4 gap-2 mb-20 h-screen overflow-y-scroll"
+    >
       {messages &&
         messages?.map((message) => (
           <ChatBubble key={message.id} message={message} />
