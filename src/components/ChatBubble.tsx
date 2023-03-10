88afc03b05062b7e418a4ff2c92d72e5f4114b33
unknown <delfinapmosqueira@gmail.com>
2023-03-10 01:13:49 -0300

    Frontend - chat App

--- src/components/ChatBubble.tsx
+++ src/components/ChatBubble.tsx
@@ -12,15 +12,15 @@ const ChatBubble = ({ message }: ChatBubbleType) => {
     <div
       className={`${
         message?.source !== VITE_APP_SERVER_NAME
-          ? 'bg-slate-400 self-end text-slate-800 rounded-tr-3xl rounded-tl-3xl rounded-bl-3xl'
-          : 'bg-blue-500 self-start text-neutral-50 rounded-tr-3xl rounded-tl-3xl rounded-br-3xl'
+          ? 'bg-slate-400 self-end text-slate-800 rounded-tr-3xl rounded-tl-3xl rounded-bl-3xl pl-6'
+          : 'bg-blue-500 self-start text-neutral-50 rounded-tr-3xl rounded-tl-3xl rounded-br-3xl pr-6'
       }
       
       max-w-[80%] px-3 py-2  flex flex-col`}
     >
       <p className="ext-m font-semibold">{message?.body}</p>
 
-      <span className="text-gray-700 text-xs  ml-auto">
+      <span className="text-gray-700 text-xs font-['Jost'] ml-auto">
         {toDateString(message?.date)}
       </span>
     </div>
